29e4a1fcddab755f11f0cade85146de7b815d9db
Malva37 <irynapysanko@gmail.com>
2020-10-01 15:30:21 +0300

    finished responsive web for members-par(mobile size)

--- js/script.js
+++ js/script.js
@@ -9,9 +9,11 @@ let membersContainer = document.querySelector('.members');
 let membersTrack = document.querySelector('.members_track');
 let members = document.querySelectorAll('.member');
 let member = document.querySelector('.member');
+let memberMobile = document.querySelectorAll('.member_mobile');
 let prevMember = document.querySelector('.prev_member');
 let nextMember = document.querySelector('.next_member');
 let headerItems = document.querySelectorAll('.item_header');
+console.log(headerItems);
 
 let memberCount = members.length;
 let coefficientMarginMember = 0.016;
@@ -20,15 +22,43 @@ let specWidthMember = ((marginLeftMember * slideToShow) - marginLeftMember) / sl
 let memberWidth = (membersContainer.clientWidth / slideToShow) - specWidthMember;
 let movePositionMember = slideToScroll * memberWidth;
 
-members.forEach((element, index) => {
-    if (index == 0) {
-        element.style.minWidth = `${memberWidth}px`;
-        element.style.marginLeft = '0';
-    } else {
-        element.style.minWidth = `${memberWidth}px`;
-        element.style.marginLeft = `${marginLeftMember}px`;
+
+window.addEventListener("resize", function () {
+    findMarginAndWidthSlide();
+})
+
+findMarginAndWidthSlide();
+
+
+function findMarginAndWidthSlide() {
+    let width = window.innerWidth;
+    if (width >= 992 && width <= 1920) {
+        slideToShow = 3;
+        coefficientMarginMember = 0.016;
+    } else if (width >= 767 && width <= 991) {
+        slideToShow = 2;
+        coefficientMarginMember = 0.016;
+    } else if (width <= 767) {
+        return
     }
-});
+    marginLeftMember = membersContainer.clientWidth * coefficientMarginMember; // placeWidth = containerPlaces.width() / slideToShowPlaces;
+    specWidthMember = ((marginLeftMember * slideToShow) - marginLeftMember) / slideToShow;
+    memberWidth = (membersContainer.clientWidth / slideToShow) - specWidthMember;
+    movePositionMember = slideToScroll * memberWidth;
+    setSliders();
+}
+
+function setSliders() {
+    members.forEach((element, index) => {
+        if (index == 0) {
+            element.style.minWidth = `${memberWidth}px`;
+            element.style.marginLeft = '0';
+        } else {
+            element.style.minWidth = `${memberWidth}px`;
+            element.style.marginLeft = `${marginLeftMember}px`;
+        }
+    });
+}
 
 prevMember.addEventListener('click', () => {
     positionMember += movePositionMember;
@@ -44,6 +74,7 @@ nextMember.addEventListener('click', () => {
     checkBtn();
     let currentActive = document.querySelector('.member+.active');
     currentActive.className = 'member';
+    // ISSUE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     currentActive.nextElementSibling.className += ' active'
 })
 
@@ -63,8 +94,19 @@ let checkActive = function (list) {
     });
 }
 
+let checkActiveMobile = function (list) {
+    list.forEach(element => {
+        element.addEventListener('click', () => {
+            element.classList.toggle('active')
+        })
+    });
+}
+
 checkActive(members);
+checkActiveMobile(memberMobile);
 checkActive(headerItems);
+
+
 
 
 let checkBtn = () => {
@@ -79,7 +121,8 @@ $(document).ready(function () {
     $('.conserts').slick({
         arrows: false,
         dots: true,
-        infinite: false
+        infinite: false,
+        adaptiveHeight: true,
 
     });
     $('.videos').slick({
@@ -88,9 +131,33 @@ $(document).ready(function () {
         initialSlide: 1
 
     });
+    $('.members_mobile').slick({
+        slidesToShow: 1,
+    }).on('beforeChange', function(event, slick,currentSlide,nextSlide){
+      currentSlide.removeClass('active')
+        console.log(nextSlide.className);
+;
+    })
+
+    $(window).on('resize orientationchange', function () {
+        $('.conserts').slick('resize');
+    });
 
 
 
+    //                  menu burger
+
+    $('.menu_burger_header').click(function () {
+        $('.menu_header, .menu_burger_header').toggleClass('active')
+    })
+
+
+    //                  spoilet list_songs
+
+
+    $('.list_songs').click(function () {
+        $(this).toggleClass('active').next().slideToggle(300);
+    })
     //                  video player
 
 
